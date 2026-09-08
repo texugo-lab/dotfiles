bc7a70381eb7c96f4dd648e7f56ee34da32fe23f
Texugo <daniel.teixeira.2910@gmail.com>
2026-09-08 00:11:14 -0300

    Update alias.zsh

--- .zscripts/alias.zsh
+++ .zscripts/alias.zsh
@@ -5,6 +5,6 @@ alias lv="la -1"
 autoload zmv
 alias src='source $HOME/.zshrc'
 alias batp="bat -p -n --paging=never --color=always"
-alias fzfp='fzf --preview="bat -p --color=always {}" --preview-border="sharp" --preview-window=right:75%'
 alias tm="tmuxifier"
 alias hstReset="rm -rf $HISTFILE && touch $HISTFILE"
+alias nvimp="fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
