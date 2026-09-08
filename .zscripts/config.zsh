140cc384cb60fe90a381c35f7d95b0b9e7d870dc
Texugo <daniel.teixeira.2910@gmail.com>
2026-09-08 00:34:49 -0300

    Update config.zsh

--- .zscripts/config.zsh
+++ .zscripts/config.zsh
@@ -18,7 +18,7 @@ export VISUAL='nvim' # VISUAL
 # TMUXIFIER
 export TMUXIFIER_LAYOUT_PATH="$HOME/.tmux-layouts"
 
-export FZF_DEFAULT_OPTS="--preview="bat -p --color=always {}" --reverse --preview-border="sharp" --preview-window=right:75% --border"
+export FZF_DEFAULT_OPTS="--preview="bat -p --color=always {}" --style=full:line --border=bold --info=hidden --pointer=> --marker=× --input-border=line --preview-border=inline --border-label-pos=18 --input-label-pos=15 --list-label-pos=16 --header-label-pos=22 --preview-window=up"
 
 #  HISTORY
 HISTSIZE=5000
