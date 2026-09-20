bc8b3224f38fd35c1b183db00b4eb980001bbe58
texugo-lab <daniel.teixeira.2910@gmail.com>
2026-09-20 20:26:38 -0300

    main session to fltc session

--- .tmux-layouts/fltc.session.sh
+++ .tmux-layouts/fltc.session.sh
@@ -1,10 +1,14 @@
-session_root "$HOME"
 if initialize_session "main"; then
   new_window "Window"
+  run_cmd "z fltc"
   new_window "Window"
+  run_cmd "z fltc"
   select_window 1
+  tmux split-pane -h
+  tmux resize-pane -R 40
+  run_cmd "z fltc"
+  tmux select-pane -L
   run_cmd "$EDITOR"
-  select_window 2
 fi
 
 finalize_and_go_to_session
